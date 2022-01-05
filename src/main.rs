ee9352f393c87dd46c0a3ea58c2fe863c2f5a922
talves <tony@3alves.com>
2022-01-05 14:26:54 -0800

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
-    // cargo run searchstring example-filename.txt
+    // cargo run searchstring poem.txt
     let args: Vec<String> = env::args().collect();
     // capture our arguments in variables
     let query = &args[1];
     let filename = &args[2];
-    println!("{:?}", args);
+    println!("Searching for '{}'", query);
+    println!("In file {}", filename);
     let mut count = 0;
-    for argument in args {
+    for argument in &args {
         println!("{}: {}", count, argument);
         count += 1;
     }
+
+    // read in the file
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
