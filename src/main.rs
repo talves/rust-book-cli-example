a63962f7ce99a645993b22f179b4c9326c326703
talves <tony@3alves.com>
2022-01-05 13:45:49 -0800

    capture arguments: display

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
+    // cargo run searchstring example-filename.txt
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+    let mut count = 0;
+    for argument in args {
+        println!("{}: {}", count, argument);
+        count += 1;
+    }
 }
