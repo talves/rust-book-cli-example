f5e5c7ea747a2c10bb90d35c99a52984866db884
talves <tony@3alves.com>
2022-01-06 12:56:51 -0800

    Errors: output (eprintln) an error message and std output

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
 
     // made and used a constructor instead
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Fatal Error: {}", err);
+        eprintln!("Fatal Error: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
