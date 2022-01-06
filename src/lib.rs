d7f6a6b883b8d4fd73c84ef8d888ca6e1e2f201e
talves <tony@3alves.com>
2022-01-06 15:45:01 -0800

    move to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::thread;
+use std::time::Duration;
 use std::{env, fs};
 
 pub struct Config {
@@ -80,6 +82,60 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     }
 
     results
+}
+
+// definition of the Cacher struct that holds a closure and an optional result value
+// see ch. 13.1 of rust book https://doc.rust-lang.org/book/ch13-01-closures.html
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_closure = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure.value(intensity));
+        println!("Next, do {} situps!", expensive_closure.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_closure.value(intensity)
+            );
+        }
+    }
 }
 
 // Tests!!!
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::process;
-use std::thread;
-use std::time::Duration;
 
+use rust_book_cli_example::generate_workout;
 use rust_book_cli_example::{run, Config};
 
 fn main() {
@@ -22,59 +21,5 @@ fn main() {
         eprintln!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-// definition of the Cacher struct that holds a closure and an optional result value
-// see ch. 13.1 of rust book https://doc.rust-lang.org/book/ch13-01-closures.html
-struct Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    calculation: T,
-    value: Option<u32>,
-}
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_closure = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure.value(intensity));
-        println!("Next, do {} situps!", expensive_closure.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_closure.value(intensity)
-            );
-        }
     }
 }
