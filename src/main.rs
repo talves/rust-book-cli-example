7ae466cda3b3824eb7720e3b1c4b385fa86de1f5
talves <tony@3alves.com>
2022-01-06 13:16:24 -0800

    Example of a wait thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::process;
+use std::thread;
+use std::time::Duration;
 
 use rust_book_cli_example::{run, Config};
 
 fn main() {
     // cargo run searchstring poem.txt
     let args: Vec<String> = env::args().collect();
+
+    simulated_expensive_calculation(20);
 
     // made and used a constructor instead
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -18,4 +22,10 @@ fn main() {
 
         process::exit(1);
     }
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
