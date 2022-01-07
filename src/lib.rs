9d808e84caec116523324ba7444d12e05ba41cd4
talves <tony@3alves.com>
2022-01-07 09:49:32 -0800

    iterators: custom iterator

--- src/lib.rs
+++ src/lib.rs
@@ -249,6 +249,15 @@ Trust me.";
         eprintln!("count: {}", counter.count);
         counter.next();
         eprintln!("count: {}", counter.count);
+
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
     }
 }
 
