5ced4db686181097939aea91865bb8df318178d4
talves <tony@3alves.com>
2022-01-07 09:46:32 -0800

    iterators: custom iterator

--- src/lib.rs
+++ src/lib.rs
@@ -169,6 +169,87 @@ Trust me.";
             search_case_insensitive(query, contents)
         );
     }
+
+    // Iterators
+    // pub trait Iterator {
+    //     type Item;
+
+    //     fn next(&mut self) -> Option<Self::Item>;
+    // }
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter(); // adds iterator trait
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+        eprintln!("v1_iter: {:?}", v1_iter);
+        let sum_value: i32 = v1_iter.sum();
+        eprintln!("v1_iter sum: {:?}", sum_value);
+        eprintln!("v1: {:?}", v1);
+    }
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+    #[test]
+    fn iterator_map() {
+        let v1: Vec<i32> = vec![1, 2, 3];
+
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+    #[test]
+    fn test_custom_iterator() {
+        let mut counter = Counter::new();
+        eprintln!("count: {}", counter.count);
+        counter.next();
+        eprintln!("count: {}", counter.count);
+    }
 }
 
 pub fn test_iterators() {
@@ -180,34 +261,34 @@ pub fn test_iterators() {
         println!("Got: {}", val);
     }
 }
-// Iterators
-// pub trait Iterator {
-//     type Item;
 
-//     fn next(&mut self) -> Option<Self::Item>;
-// }
-#[test]
-fn iterator_demonstration() {
-    let v1 = vec![1, 2, 3];
-
-    let mut v1_iter = v1.iter(); // adds iterator trait
-
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
-    eprintln!("v1_iter: {:?}", v1_iter);
-    let sum_value: i32 = v1_iter.sum();
-    eprintln!("v1_iter sum: {:?}", sum_value);
-    eprintln!("v1: {:?}", v1);
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
-#[test]
-fn iterator_sum() {
-    let v1 = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
 
-    let total: i32 = v1_iter.sum();
+struct Counter {
+    count: u32,
+}
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+impl Iterator for Counter {
+    type Item = u32;
 
-    assert_eq!(total, 6);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
 }
